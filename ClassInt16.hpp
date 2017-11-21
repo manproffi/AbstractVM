c39185c2c9efdabec537f4c7078560004accc22e
manproffi <prosergii@ukr.net>
2017-11-21 14:50:53 +0200

    new_some_text_for_project

--- ClassInt16.hpp
+++ ClassInt16.hpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <limits.h>
 
-class Int8 : public IOperand
+class Int16 : public IOperand
 {
 
 private:
-	char			_value;
+	short			_value;
 	eOperandType	eType;
 
-	Int8();
-	Int8 (Int8 const & rhs);
-	Int8 operator= (Int8 const & rhs);
+	Int16();
+	Int16 (Int16 const & rhs);
+	Int16 operator= (Int16 const & rhs);
 
 public:
 	class OverflowException(): public std::exception
@@ -26,10 +26,10 @@ public:
 	};
 
 
-	explicit Int8(char a);
-	~Int8();
+	explicit Int16(char a);
+	~Int16();
 
-	char			getValue(void) const;
+	short			getValue(void) const;
 
 	int 			getPrecision(void) const;
 	eOperandType 	getType(void) const;
@@ -44,27 +44,27 @@ public:
 
 };
 
-const char* Int8::OverflowException::what() const throw()
+const char* Int16::OverflowException::what() const throw()
 {
-    return ("overflow Int8");  
+    return ("overflow Int16");  
 }
 
-const char* Int8::UnderflowException::what() const throw()
+const char* Int16::UnderflowException::what() const throw()
 {
-    return ("underflow Int8");  
+    return ("underflow Int16");  
 }
 
-Int8::Int8(char a) : _value(a), eType(Int8) {}
+Int16::Int16(char a) : _value(a), eType(Int16) {}
 
-Int8::~Int8() {}
+Int16::~Int16() {}
 
-char			Int8::getValue(void) const { return _value; }
+short			Int16::getValue(void) const { return _value; }
 
-int 	Int8::getPrecision(void) const { return eOperandType::Int8; }
+int 			Int16::getPrecision(void) const { return eOperandType::Int16; }
 
-eOperandType Int8::getType(void) const { return eType; }
+eOperandType 	Int16::getType(void) const { return eType; }
 
-IOperand const * Int8::operator + (IOperand const & rhs) const
+IOperand const * Int16::operator + (IOperand const & rhs) const
 {
 	// int_type a = 12356, b = 98765432;
 	// if ( b > 0 && a > std::numeric_limits< int_type >::max() - b )
@@ -90,7 +90,7 @@ IOperand const * Int8::operator + (IOperand const & rhs) const
 		;
 	}*/
 
-	std::cout << "=========== Int8 operator+ START" << std::endl;
+	std::cout << "=========== Int16 operator+ START" << std::endl;
 	eOperandType 	type;
 	IOperand		*newObj = nullptr;
 
@@ -98,19 +98,19 @@ IOperand const * Int8::operator + (IOperand const & rhs) const
 	
 	if (type == getType())
 	{
-		if ( _value > 0 && rhs.getValue() > std::numeric_limits<char>::max() - _value)
-			throw Int8::OverflowException(); 
+		if ( _value > 0 && static_cast<short>(rhs.getValue()) > std::numeric_limits<short>::max() - _value)
+			throw Int16::OverflowException(); 
 
-		if (_value < 0 && rhs.getValue() <  std::numeric_limits<char>::min() - _value)
-			throw Int8::UnderflowException();
+		if (_value < 0 && static_cast<short>(rhs.getValue()) <  std::numeric_limits<short>::min() - _value)
+			throw Int16::UnderflowException();
 		
-		newObj = new Int8(_value + rhs.getValue());
+		newObj = new Int16(_value + static_cast<short>(rhs.getValue()));
 		
 		return newObj;
 	}
 	else
 	{
-		std::cout << "=========== Int8 operator+ " << std::endl;
+		std::cout << "=========== Int16 operator+ " << std::endl;
 		
 		newObj = FactoryMethod.createOperand(type, std::to_string(static_cast<int>(getValue())));
 		newObj = newObj + rhs;
